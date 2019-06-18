60feb88a58eb445de12790b9b4f182fe919192f8
v_tianzhiyuan <v_tianzhiyuan@baidu.com>
2019-06-18 13:46:16 +0800

    导qheroiqhorihqoi

--- lib/pages/my/my_page.dart
+++ lib/pages/my/my_page.dart
@@ -6,7 +6,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class MyInfoPage extends StatelessWidget {
+class MyInfoPage extends StatefulWidget {
+  @override
+  _MyInfoPageState createState() => _MyInfoPageState();
+}
+
+class _MyInfoPageState extends State<MyInfoPage> {
   static const double IMAGE_ICON_WIDTH = 30.0;
   static const double ARROW_ICON_WIDTH = 16.0;
 
@@ -45,10 +50,33 @@ class MyInfoPage extends StatelessWidget {
     height: ARROW_ICON_WIDTH,
   );
 
+  ScrollController _scrollController = ScrollController();
+
+  bool hidden = false;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    _scrollController.addListener(() {
+      if (_scrollController.offset > 120) {
+        setState(() {
+          hidden = true;
+        });
+      } else {
+        setState(() {
+          hidden = false;
+        });
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return new NestedScrollView(
         headerSliverBuilder: _headerSliverBuilder,
+        controller: _scrollController,
         reverse: false,
         body: new ListView(
           children: titles.map((v) {
@@ -81,8 +109,7 @@ class MyInfoPage extends StatelessWidget {
         ));
   }
 
-  List<Widget> _headerSliverBuilder(
-      BuildContext context, bool innerBoxIsScrolled) {
+  List<Widget> _headerSliverBuilder(BuildContext context, bool innerBoxIsScrolled) {
     return <Widget>[
       SliverAppBar(
         //1.在标题左侧显示的一个控件，在首页通常显示应用的 logo；在其他界面通常显示为返回按钮
@@ -91,7 +118,7 @@ class MyInfoPage extends StatelessWidget {
         automaticallyImplyLeading: true,
         elevation: 4,
         //APP bar 的颜色，默认值为 ThemeData.primaryColor。改值通常和下面的三个属性一起使用
-//        backgroundColor: Colors.pink,
+        backgroundColor: Colors.pink,
         //App bar 的亮度，有白色和黑色两种主题，默认值为 ThemeData.primaryColorBrightness
         brightness: Brightness.light,
         //App bar 上图标的颜色、透明度、和尺寸信息。默认值为 ThemeData().primaryIconTheme
@@ -99,35 +126,48 @@ class MyInfoPage extends StatelessWidget {
         //App bar 上的文字主题。默认值为 ThemeData（）.primaryTextTheme
         textTheme: ThemeData().accentTextTheme,
         //此应用栏是否显示在屏幕顶部
-        primary: false,
+        primary: true,
         //标题是否居中显示，默认值根据不同的操作系统，显示方式不一样,true居中 false居左
         centerTitle: true,
         //横轴上标题内容 周围的间距
         titleSpacing: NavigationToolbar.kMiddleSpacing,
         //展开高度
-        expandedHeight: 170,
+        expandedHeight: 120,
         //是否随着滑动隐藏标题
         floating: true,
         //tab 是否固定在顶部
         pinned: true,
         //与floating结合使用
         snap: true,
-
+        actions: <Widget>[
+          new IconButton(
+            // action button
+            icon: new Icon(Icons.add),
+            onPressed: () {},
+          ),
+          new IconButton(
+            // action button
+            icon: new Icon(Icons.add),
+            onPressed: () {},
+          ),
+        ],
         //当前界面的标题文字
-        title: Text('当前界面的标题文字'),
+        title: hidden ? Text('我的页面') : null,
         //5.一个显示在 AppBar 下方的控件，高度和 AppBar 高度一样，
         // 可以实现一些特殊的效果，该属性通常在 SliverAppBar 中使用
         flexibleSpace: FlexibleSpaceBar(
           centerTitle: true,
-          background: _userCardInfo(),
+          background: UserCardInfo(),
         ),
-        //7.? 材料设计中控件的 z 坐标顺序，默认值为 4，对于可滚动的 SliverAppBar，
-        // 当 SliverAppBar 和内容同级的时候，该值为 0， 当内容滚动 SliverAppBar 变为 Toolbar 的时候，修改 elevation 的值
       )
     ];
   }
+}
 
-  Widget _userCardInfo() {
+/// 用户基本信息
+class UserCardInfo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.only(top: 20),
       width: ScreenUtil().setWidth(750),
@@ -136,7 +176,7 @@ class MyInfoPage extends StatelessWidget {
           /// 左侧按钮
           Expanded(
             child: Container(
-              margin: EdgeInsets.only(top: 20, left: 15),
+              margin: EdgeInsets.only(top: 18, left: 15),
               child: Column(
                 children: <Widget>[
                   /// 头像 昵称 邀请码
@@ -147,11 +187,16 @@ class MyInfoPage extends StatelessWidget {
                         // 头像
                         Container(
                           alignment: Alignment.topRight,
+                          decoration: BoxDecoration(
+                              border: Border.all(width: 0.50, color: Colors
+                                  .white),
+                              borderRadius: BorderRadius.circular(50)
+                          ),
                           child: CircleAvatar(
                             radius: 30,
                             backgroundImage: new NetworkImage(
                               'http://e.hiphotos.baidu'
-                              '.com/image/pic/item/359b033b5bb5c9eac1754f45df39b6003bf3b396.jpg',
+                                  '.com/image/pic/item/359b033b5bb5c9eac1754f45df39b6003bf3b396.jpg',
                             ),
                           ),
                         ),
@@ -177,7 +222,7 @@ class MyInfoPage extends StatelessWidget {
                                       decoration: BoxDecoration(
                                           color: Colors.black54,
                                           borderRadius:
-                                              BorderRadius.circular(10)),
+                                          BorderRadius.circular(10)),
                                       margin: EdgeInsets.only(left: 5),
                                       padding: EdgeInsets.only(
                                           left: 5, right: 5, bottom: 1),
@@ -222,7 +267,7 @@ class MyInfoPage extends StatelessWidget {
                                         decoration: BoxDecoration(
                                             color: Colors.black54,
                                             borderRadius:
-                                                BorderRadius.circular(10)),
+                                            BorderRadius.circular(10)),
                                         margin: EdgeInsets.only(left: 5),
                                         padding: EdgeInsets.only(
                                             left: 5, right: 5, bottom: 1),
@@ -305,30 +350,6 @@ class MyInfoPage extends StatelessWidget {
               ),
             ),
             flex: 8,
-          ),
-
-          /// 右侧按钮
-          Expanded(
-            child: Container(
-              alignment: Alignment.topRight,
-              child: Row(
-                children: <Widget>[
-                  Container(
-                    child: IconButton(
-                      icon: new Icon(Icons.add),
-                      onPressed: () {},
-                    ),
-                  ),
-                  Container(
-                    child: IconButton(
-                      icon: new Icon(Icons.add),
-                      onPressed: () {},
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            flex: 3,
           ),
         ],
       ),
